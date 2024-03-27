5a8e5dafc5a7039d10b28104d8152876fbebcd84
lineshape <akmsuh903@gmail.com>
2024-03-27 13:31:55 +0900

    firebase

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 // API 키와 다른 필요한 파라미터를 변수로 설정
 const API_KEY = 'kB6%2BuNd91DO08ONrRhHgoM5duj6ujcrQGpQWh3GnGwX%2F00uYw32OrCV5Ev1FVowqgOcJdc60n%2Bhjwb4hJ5H0MA%3D%3D';
-const SERVICE_URL = 'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst';
+const SERVICE_URL = 'https://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst';
 const numOfRows = 10; // 예를 들어, 가져오고 싶은 행의 수
 const pageNo = 1; // 예를 들어, 페이지 번호
 const itemCode = 'PM10'; // 조회하고 싶은 항목 (PM10, PM2.5 등)
@@ -12,6 +12,48 @@ const returnType = 'json';
 // 전체 URL 조합
 const url = `${SERVICE_URL}?serviceKey=${API_KEY}&numOfRows=${numOfRows}&pageNo=${pageNo}&itemCode=${itemCode}&dataGubun=${dataGubun}&searchCondition=${searchCondition}&sidoName=${encodeURIComponent(sidoName)}&returnType=${returnType}`;
 
+// 포스터 배경 인터랙션 또는 애니메이션
+
+const risingHeart = (count) => {
+
+    const background = document.querySelector('.poster.background.clear');
+
+    for(let i = 0; i < count; i++) {
+        
+        const heart = document.createElement('span');
+
+        setTimeout(() => {
+            heart.innerText = '♡';
+            heart.style.left = Math.random() * 100 + 'vw';
+            heart.style.bottom = '-300px';
+            background.appendChild(heart);
+
+            heart.style.animation = 'heart-rise 7s ease-out forwards';
+
+            heart.addEventListener('animationend', function() {
+                heart.remove();
+            })
+        }, i * 1300);
+        
+    }
+    
+}
+
+const createHeart = (e) => {
+
+    const background = document.querySelector('.poster.background.clear');
+    const heart = document.createElement('span');
+
+    heart.innerText = '♡';
+    heart.style.left = `${e.x - window.innerWidth * 0.03}px`;
+    heart.style.top = `${e.y - window.innerHeight * 0.03}px`;
+    background.appendChild(heart);
+
+    heart.style.animation = 'heart-rise 7s ease-out forwards';
+    heart.addEventListener('animationend', function() {
+        heart.remove();})
+    }
+
 
 fetch(url)
     .then(response => response.json())
@@ -19,7 +61,6 @@ fetch(url)
         // API를 통해 받아온 데이터를 'res' 변수에 담는다
         const res = data;
         const seoulData = res.response.body.items[0].seoul;
-        // const seoulData = 90;
         const dateData = res.response.body.items[0].dataTime;
 
         // Date 객체 생성
@@ -37,6 +78,8 @@ fetch(url)
             document.querySelectorAll("h2")[0].innerText = "Clear";
             document.querySelectorAll("h2")[1].innerText = "Clear";
             document.querySelector(".dust-grade").innerText = "좋음";
+            risingHeart(5000);
+            document.addEventListener("click", createHeart);
         } else if(31 <= seoulData && seoulData < 81) {
             document.querySelector("html").dataset.dust="normal";
             document.querySelectorAll("h2")[0].innerText = "Normal";
@@ -66,6 +109,6 @@ fetch(url)
 
 const pos = document.documentElement;
 pos.addEventListener('mousemove', e => {
-    pos.style.setProperty('--x', `${e.clientX}px`);
-    pos.style.setProperty('--y', `${e.clientY}px`);
+    pos.style.setProperty('--x', `${e.x}px`);
+    pos.style.setProperty('--y', `${e.y}px`);
 })
